0ca6f0847bb1e64cab90c373a9f69be9bf52ac35
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-03-03 02:19:46 +0300

    implement

--- main.go
+++ main.go
@@ -1,21 +1,35 @@
 package main
 
 import (
+	"os"
 	"fmt"
+	"strconv"
 	"github.com/miekg/dns"
 )
 
 func main() {
-	// add dns handlers
+	port := 5354
+
+	if len(os.Args) > 1 {
+		aport, err := strconv.Atoi(os.Args[1])
+
+		if err != nil {
+			panic("can not parse port")
+		}
+
+		port = aport
+	}
+
 	handler := func(w dns.ResponseWriter, r *dns.Msg) {
 		m := createDNSReply(r)
 		m.SetReply(r)
 		w.WriteMsg(m)
 	}
+
 	dns.HandleFunc("local.", handler)
-	// start DNS server
+
 	dserver := &dns.Server{
-		Addr: fmt.Sprintf("%s:%d", "localhost", 5354),
+		Addr: fmt.Sprintf("%s:%d", "localhost", port),
 		Net:  "udp",
 	}
 	defer dserver.Shutdown()
--- query.go
+++ query.go
@@ -2,15 +2,74 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"errors"
+	"strings"
+	"os/exec"
 	"github.com/miekg/dns"
 )
 
-func resolve(name string, recordType string) (dns.RR, error) {
-	return dns.NewRR(fmt.Sprintf("%s %s %s", name, recordType, 0))
+func parseLine(data string) (string, error) {
+	if !strings.Contains(data, ".local.") {
+		return "", errors.New("malformed record")
+	}
+
+	fields := strings.Split(data, " ")
+
+	if len(fields) < 1 {
+		return "", errors.New("malformed record")
+	}
+
+	real := []string{}
+
+	for _, f := range fields {
+		if len(f) > 0 {
+			real = append(real, f)
+		}
+	}
+
+	if len(real) < 2 {
+		return "", errors.New("malformed record")
+	}
+
+	return real[len(real) - 2], nil
+}
+
+func parse(data string) (string, error) {
+	for _, l := range strings.Split(data, "\n") {
+		addr, err := parseLine(l)
+
+		if err == nil {
+			return addr, nil
+		}
+	}
+
+	return "", fmt.Errorf("malformed result %s, %w", data, errors.New("parse error"))
+}
+
+func resolve(name string, ver int, recType string) (dns.RR, error) {
+	out, err := exec.Command("dns-sd", "-t", "1", "-m", "-G", fmt.Sprintf("v%d", ver), name).Output()
+
+	if err != nil {
+		return nil, err
+	}
+
+	addr, err := parse(string(out))
+
+	if err != nil {
+		return nil, err
+	}
+
+	if strings.Contains(addr, "%") {
+		return nil, fmt.Errorf("bad address %s, %w", addr, errors.New("link local"))
+	}
+
+	return dns.NewRR(fmt.Sprintf("%s %s %s", name, recType, addr))
 }
 
 func createDNSReply(r *dns.Msg) *dns.Msg {
 	m := new(dns.Msg)
+
 	m.Compress = false
 
 	switch r.Opcode {
@@ -18,18 +77,22 @@ func createDNSReply(r *dns.Msg) *dns.Msg {
 		for _, q := range r.Question {
 			switch q.Qtype {
 			case dns.TypeA:
-				rr, err := resolve(q.Name, "A")
-				if err != nil {
-					continue
+				rr, err := resolve(q.Name, 4, "A")
+
+				if err == nil {
+					m.Answer = append(m.Answer, rr)
+				} else {
+					log.Println(err)
 				}
-				m.Answer = append(m.Answer, rr)
 
 			case dns.TypeAAAA:
-				rr, err := resolve(q.Name, "AAAA")
-				if err != nil {
-					continue
+				rr, err := resolve(q.Name, 6, "AAAA")
+
+				if err == nil {
+					m.Answer = append(m.Answer, rr)
+				} else {
+					log.Println(err)
 				}
-				m.Answer = append(m.Answer, rr)
 
 			default:
 				continue
